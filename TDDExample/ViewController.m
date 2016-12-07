ec2608762b568842f0469ccd343970bdfa58b4f8
fuyoufang <frederiefu@zhen22.com>
2016-12-07 20:53:00 +0800

    init

---
+++ TDDExample/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TDDExample
+//
+//  Created by fuyoufang on 2016/12/7.
+//  Copyright © 2016年 fyf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
